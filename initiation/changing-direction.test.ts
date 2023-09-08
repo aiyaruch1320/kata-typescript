93f779a412426ee6707685f6d41b07aa099735f0
Aiyaruch <aiyaruch1320@hotmail.com>
2023-09-08 11:48:41 +0700

    add test case and make it pass

--- initiation/changing-direction.test.ts
+++ initiation/changing-direction.test.ts
@@ -4,27 +4,25 @@ describe("changingDirection", () => {
     expect(changingDirection([1, 2, 3, 2, 1])).toEqual(1);
     expect(changingDirection([1, 2, 2, 1, 2, 2])).toEqual(2);
     expect(changingDirection([1, 2, 3, 5, 4, 2, 5, 7, 8, 3, 2, 1])).toEqual(3);
+    expect(changingDirection([4, 3, 2, 3])).toEqual(1);
+    expect(
+      changingDirection([6, 6, 6, 4, 1, 2, 5, 9, 7, 8, 5, 9, 4, 2, 6])
+    ).toEqual(7);
+    expect(changingDirection([3, 3, 3, 4])).toEqual(0);
   });
 });
 
 function changingDirection(nums: number[]): number {
   let directionCount = 0;
-  let direction = Direction.Up;
+  let directionTrend = 0;
 
   for (let i = 1; i < nums.length; i++) {
-    if (
-      (nums[i] > nums[i - 1] && direction === Direction.Down) ||
-      (nums[i] < nums[i - 1] && direction === Direction.Up)
-    ) {
-      directionCount++;
-      direction = direction === Direction.Up ? Direction.Down : Direction.Up;
-    }
+    let different = nums[i] - nums[i - 1];
+    let currentTrend = different > 0 ? 1 : -1;
+    if (different === 0 || directionTrend === currentTrend) continue;
+    if (directionTrend !== 0) directionCount++;
+    directionTrend = currentTrend;
   }
 
   return directionCount;
-}
-
-enum Direction {
-  Up = "up",
-  Down = "down",
 }
