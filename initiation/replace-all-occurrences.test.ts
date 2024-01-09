d2b09acb613e653bb548c2b7517ade740b6d0593
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-01-09 09:54:53 +0700

    [replace-all-occurrences] change tests

--- initiation/replace-all-occurrences.test.ts
+++ initiation/replace-all-occurrences.test.ts
@@ -1,14 +1,16 @@
-describe("countVowels", () => {
-  it("should return number of vowels in sentense", () => {
-    expect(replaceAll("hello world", "world", "TypeScript")).toEqual(
-      "hello TypeScript"
-    );
+describe("replaceAll", () => {
+  it("should return a new string where all occurrences of the target substring within the main text are replaced with the replacement substring", () => {
     expect(replaceAll("hello world world", "world", "TypeScript")).toEqual(
       "hello TypeScript TypeScript"
     );
-    expect(replaceAll("TypeScript is fun", "fun", "awesome")).toEqual(
-      "hello TypeScript TypeScript"
+    expect(replaceAll("aaa", "a", "b")).toEqual("bbb");
+    expect(replaceAll("replace all the all", "all", "some")).toEqual(
+      "replace some the some"
     );
+    expect(replaceAll("nothing to replace", "something", "nothing")).toEqual(
+      "nothing to replace"
+    );
+    expect(replaceAll("", "anything", "nothing")).toEqual("");
   });
 });
 
